95e4c139af15acad53935862e6f2a812d98f86b2
Joshua <joshuaystang@gmail.com>
2019-07-30 21:25:39 +1000

    Added to scan for link of a file

--- group_defender/defend/file.py
+++ group_defender/defend/file.py
@@ -36,7 +36,7 @@ def process_file(update, context):
     # Check if file is too large for bot to download
     if file_size > MAX_FILESIZE_DOWNLOAD:
         if update.message.chat.type == Chat.PRIVATE:
-            text = f'Your {file_type} is too large for me to download and process.'
+            text = f'Your {file_type} is too large for me to download and check.'
             update.message.reply_text(text)
 
         return
@@ -53,17 +53,14 @@ def process_file(update, context):
 
 def check_file(update, context, file_name, file_type):
     update.message.chat.send_action(ChatAction.TYPING)
-    status, matches = scan_file(file_name)
+    is_safe, status, matches = scan_file(file_name)
     chat_type = update.message.chat.type
     chat_id = update.message.chat_id
     msg_id = update.message.message_id
     user_name = update.message.from_user.first_name
     msg_text = update.message.text
 
-    if status == OK:
-        if chat_type == Chat.PRIVATE:
-            update.message.reply_text('I think it doesn\'t contain any virus or malware.')
-    elif status in (FOUND, WARNING):
+    if not is_safe:
         threat_type = 'contains' if status == FOUND else 'may contain'
         if chat_type in (Chat.GROUP, Chat.SUPERGROUP):
             # store_msg(chat_id, msg_id, user_name, file_id, file_type, msg_text)
@@ -76,27 +73,37 @@ def check_file(update, context, file_name, file_type):
             context.bot.send_message(chat_id, text, reply_markup=reply_markup)
         else:
             update.message.reply_text(f'I think it {threat_type} a virus or malware, don\'t download or open it.')
-    elif status == PENDING:
-        keyboard = [[InlineKeyboardButton(text='Try again', callback_data=f'again,{msg_id}')]]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-
-        update.message.reply_text(f'I am still scanning this {file_type}.', reply_markup=reply_markup)
     else:
-        log = Logger()
-        log.error(matches)
+        if status == OK:
+            if chat_type == Chat.PRIVATE:
+                update.message.reply_text('I think it doesn\'t contain any virus or malware.')
+        elif status == PENDING:
+            keyboard = [[InlineKeyboardButton(text='Try again', callback_data=f'again,{msg_id}')]]
+            reply_markup = InlineKeyboardMarkup(keyboard)
 
-        keyboard = [[InlineKeyboardButton(text='Try again', callback_data=f'again,{msg_id}')]]
-        reply_markup = InlineKeyboardMarkup(keyboard)
+            update.message.reply_text(f'I am still scanning this {file_type}.', reply_markup=reply_markup)
+        else:
+            log = Logger()
+            log.error(matches)
 
-        update.message.reply_text(f'Something went wrong.', reply_markup=reply_markup)
+            keyboard = [[InlineKeyboardButton(text='Try again', callback_data=f'again,{msg_id}')]]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+
+            update.message.reply_text(f'Something went wrong.', reply_markup=reply_markup)
 
 
-def scan_file(file_name):
+def scan_file(file_name=None, file_url=None):
+    is_safe = True
     status = matches = None
-    url = 'https://beta.attachmentscanner.com/scans'
+    url = 'http://beta.attachmentscanner.com/v0.1/scans'
     headers = {'authorization': f'bearer {SCANNER_TOKEN}'}
-    files = {'file': open(file_name, 'rb')}
-    r = requests.post(url=url, headers=headers, files=files)
+
+    if file_name is not None:
+        files = {'file': open(file_name, 'rb')}
+        r = requests.post(url, headers=headers, files=files)
+    else:
+        json = {'url': file_url}
+        r = requests.post(url, headers=headers, json=json)
 
     if r.status_code == 200:
         results = r.json()
@@ -105,4 +112,7 @@ def scan_file(file_name):
         if status == FAILED:
             matches = results['matches']
 
-    return status, matches
+    if status in (FOUND, WARNING):
+        is_safe = False
+
+    return is_safe, status, matches
